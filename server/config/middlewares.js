f8ec3eb2a14a1459724730f2cc25fb2e217dcef8
peter varga <pkvarg@yahoo.se>
2023-04-16 19:45:57 +0200

    middle, server settings

--- server/config/middlewares.js
+++ server/config/middlewares.js
@@ -1,12 +1,19 @@
 module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::errors",
+  "strapi::security",
+  {
+    name: "strapi::cors",
+    config: {
+      origin: ["https://ecommerce.pictusweb.sk", "https://pic-api.click"],
+      headers: "*",
+    },
+  },
+
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
